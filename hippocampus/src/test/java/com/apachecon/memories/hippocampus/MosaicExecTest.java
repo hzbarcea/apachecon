c55ac48b5f25319f39e9112c542dc1fe049b2da8
Hadrian Zbarcea <hzbarcea@talend.com>
2011-11-03 18:27:38 -0400

    camel-exec unit test not working yet...

---
+++ hippocampus/src/test/java/com/apachecon/memories/hippocampus/MosaicExecTest.java
@@ -0,0 +1,41 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.apachecon.memories.hippocampus;
+
+import org.apache.camel.test.junit4.CamelSpringTestSupport;
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class MosaicExecTest extends CamelSpringTestSupport {
+
+    protected ClassPathXmlApplicationContext createApplicationContext() {
+        return new ClassPathXmlApplicationContext("META-INF/spring/camel-context.xml");
+    }
+
+    @Test
+    public void testMosaic() throws Exception {
+        template.sendBody("{{schedule.generate.thumbnails}}", "Hello world");
+/*
+    	MockEndpoint mock = (MockEndpoint)context.getEndpoint("mock:result");
+        mock.expectedMinimumMessageCount(1);
+        mock.assertIsSatisfied();
+*/
+        // assertTrue(mock.assertExchangeReceived(0).getIn().getBody() instanceof Tweet);
+        // s = mock.assertExchangeReceived(0).getIn().getBody(Search.class);
+    }
+}
