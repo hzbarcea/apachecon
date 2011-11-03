9d29e71d926bbcfc6a0cb13eaa3bccee9494c97c
Lukasz Dywicki <luke@code-house.org>
2011-11-03 20:32:59 +0100

    Approval service definition. Very basic support for displaying all images (TBD)

--- scrapbook/src/main/java/com/apachecon/memories/Approve.java
+++ scrapbook/src/main/java/com/apachecon/memories/Approve.java
@@ -1,5 +1,6 @@
 package com.apachecon.memories;
 
+import com.apachecon.memories.service.ImageService;
 import org.apache.wicket.authroles.authorization.strategies.role.annotations.AuthorizeInstantiation;
 
 @AuthorizeInstantiation("admin")
@@ -7,5 +8,10 @@ public class Approve extends ScrapbookPage {
     private static final long serialVersionUID = 1L;
 
     public Approve() {
+        ScrapbookApplication application = (ScrapbookApplication) getApplication();
+        //ApproveService service = application.getApprovalService();
+        ImageService service = application.getImageService();
+
+        add(new Gallery("gallery", service.getAll()));
     }
 }
