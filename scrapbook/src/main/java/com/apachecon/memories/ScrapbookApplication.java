5e20549cbb70f1fd2bd45c0b1ba3d89033ad75c6
Lukasz Dywicki <luke@code-house.org>
2011-11-04 18:49:10 +0100

    Fixed path mapping

--- scrapbook/src/main/java/com/apachecon/memories/ScrapbookApplication.java
+++ scrapbook/src/main/java/com/apachecon/memories/ScrapbookApplication.java
@@ -15,10 +15,12 @@ import java.util.Properties;
 
 import org.apache.cxf.feature.LoggingFeature;
 import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
+import org.apache.wicket.Session;
 import org.apache.wicket.authentication.strategy.NoOpAuthenticationStrategy;
 import org.apache.wicket.authroles.authentication.AbstractAuthenticatedWebSession;
 import org.apache.wicket.authroles.authentication.AuthenticatedWebApplication;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.request.mapper.BufferedResponseMapper;
 
 /**
  * Application object for your web application. If you want to run this
@@ -44,12 +46,20 @@ public class ScrapbookApplication extends AuthenticatedWebApplication {
      */
     @Override
     public void init() {
-        mountPage("/index", Index.class);
-        mountPackage("/signin", SignIn.class);
-        mountPackage("/logout", Logout.class);
-        mountPackage("/aprove", Approve.class);
-        mountPackage("/upload", Upload.class);
         super.init();
+
+        mountPage("/index.html", Index.class);
+        mountPage("/signin.html", SignIn.class);
+        mountPage("/logout.html", Logout.class);
+        mountPage("/browse.html", Browse.class);
+        mountPage("/aprove.html", Approve.class);
+        mountPage("/upload.html", Upload.class);
+
+        mount(new BufferedResponseMapper() {
+            protected String getSessionId() {
+                return Session.get().getId();
+            }
+        });
 
         // disable cookie with user/pass, it's not safe
         getSecuritySettings().setAuthenticationStrategy(new NoOpAuthenticationStrategy());
